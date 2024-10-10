59954be73b9e9ef8b1ca6569bea70543094d48bb
Brijesh Kumar <bk10895@gmail.com>
2024-10-10 13:47:59 +0530

    Refactor CSS and update App componentRefactor lint-staged configuration and package.json formattingAdd configuration files and update App componentInitial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-600">Netflix GPT</h1>
+      <h1 className="p-4 text-center text-3xl font-bold text-blue-600 underline">
+        Netflix GPT
+      </h1>
     </>
   );
 }
