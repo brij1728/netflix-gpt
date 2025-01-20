f5647c1cbff7af4cd92684ad276bad6a937a1315
Brijesh Kumar <bk10895@gmail.com>
2025-01-20 19:35:29 +0530

    feat: add THE_MOVIE_DB_URL constant and update fetchMovies to use it

--- src/api/fetchMovies.ts
+++ src/api/fetchMovies.ts
@@ -1,8 +1,7 @@
-import { API_OPTIONS } from '../utils/constants';
+import { API_OPTIONS, THE_MOVIE_DB_URL } from '../utils/constants';
 
 export const getCurrentPlayingMovies = async () => {
-  const url =
-    'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+  const url = THE_MOVIE_DB_URL;
 
   const response = await fetch(url, API_OPTIONS);
   const data = await response.json();
