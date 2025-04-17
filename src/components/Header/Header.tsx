28fc0e3a4202e213ece216aa15e5bec726145dc2
Brijesh Kumar <bk10895@gmail.com>
2025-04-17 13:05:07 +0530

    feat: conditionally render language selection in Header based on GPT search visibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,8 +12,12 @@ import { supportedLanguages } from '../../utils/languageConstants';
 import { toggleGPTSearchView } from '../../redux/slices/gptSlice';
 
 export const Header = () => {
-  const user = useSelector((store: RootState) => store.user.currentUser);
   const dispatch = useDispatch<AppDispatch>();
+
+  const user = useSelector((store: RootState) => store.user.currentUser);
+  const showGPTSearch = useSelector(
+    (store: RootState) => store.gpt.showGPTSearch
+  );
 
   const navigate = useNavigate();
 
@@ -57,29 +61,31 @@ export const Header = () => {
         <div>
           {user ? (
             <div className="flex items-center space-x-2">
-              <div className="relative flex items-center justify-between gap-2">
-                <IoLanguage className="pointer-events-none absolute left-[1px] top-1/2 mx-[2px] -translate-y-1/2 text-white-100 sm:text-xs md:mx-1 md:text-sm lg:mx-2 lg:text-base" />
-                <select
-                  className="appearance-none rounded-md bg-netflix-red px-3 py-[1px] text-[10px] text-white-100 sm:px-4 sm:text-xs md:px-6 md:py-1 md:text-sm lg:px-8 lg:text-base"
-                  onChange={handleLanguageChange}
-                >
-                  {supportedLanguages.map((lang) => (
-                    <option
-                      className="bg-white-100 text-netflix-black"
-                      key={lang.identifier}
-                      value={lang.identifier}
-                    >
-                      {lang.languageName}
-                    </option>
-                  ))}
-                </select>
-              </div>
+              {showGPTSearch && (
+                <div className="relative flex items-center justify-between gap-2">
+                  <IoLanguage className="pointer-events-none absolute left-[1px] top-1/2 mx-[2px] -translate-y-1/2 text-white-100 sm:text-xs md:mx-1 md:text-sm lg:mx-2 lg:text-base" />
+                  <select
+                    className="appearance-none rounded-md bg-netflix-red px-3 py-[1px] text-[10px] text-white-100 sm:px-4 sm:text-xs md:px-6 md:py-1 md:text-sm lg:px-8 lg:text-base"
+                    onChange={handleLanguageChange}
+                  >
+                    {supportedLanguages.map((lang) => (
+                      <option
+                        className="bg-white-100 text-netflix-black"
+                        key={lang.identifier}
+                        value={lang.identifier}
+                      >
+                        {lang.languageName}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              )}
 
               <Button
                 onClick={handleSearch}
                 className="bg-gray-500 px-1 py-[2px] text-[10px] text-white-100 hover:bg-gray-700 sm:px-2 sm:py-1 sm:text-sm md:px-4 md:py-2 md:text-base lg:text-xl"
               >
-                GPT Search
+                {showGPTSearch ? 'Home' : 'GPT Search'}
               </Button>
               <img
                 src={user.photoURL || '/default-avatar-url'}
